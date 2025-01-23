09033cee8c352c9fc4f50392e9e7b82a7d8dace8
elisa panozzo <106873581+elisozzo@users.noreply.github.com>
2025-01-24 00:28:05 +0100

    dissolvingController

--- Assets/POINT CLOUD/Scripts/dissolvingControl.cs
+++ Assets/POINT CLOUD/Scripts/dissolvingControl.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class dissolvingControl : MonoBehaviour
 {
-    private MeshRenderer mesh; // Dichiarazione del MeshRenderer
-    private Material meshMaterial; // Dichiarazione del Material
+    private MeshRenderer mesh; 
+    private Material meshMaterial; 
     public float dissolveRate = 0.0125f;
     public float refreshRate = 0.025f;
+    private Light[] sceneLights;
+    private float[] originalIntensities;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        // Recupera il MeshRenderer
         mesh = GetComponent<MeshRenderer>();
 
         if (mesh != null)
         {
-            // Recupera il materiale associato
             meshMaterial = mesh.material;
         }
         else
         {
-            Debug.LogError("Non è stato trovato alcun MeshRenderer!");
+            Debug.LogWarning("Non è stata trovata la mesh!");
         }
+
+        sceneLights = FindObjectsOfType<Light>();
+        if (sceneLights.Length == 0)
+        {
+            Debug.LogWarning("Nessuna luce trovata nella scena.");
+        }
+        else
+        {
+            originalIntensities = new float[sceneLights.Length];
+            for (int i = 0; i < sceneLights.Length; i++)
+            {
+                originalIntensities[i] = sceneLights[i].intensity;
+                sceneLights[i].intensity = 0;
+            }
+        }
+      
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
@@ -39,21 +54,39 @@ public class dissolvingControl : MonoBehaviour
     {
         if (meshMaterial == null)
         {
-            Debug.LogError("Materiale non trovato. Impossibile avviare la dissolvenza.");
+            Debug.LogError("Materiale non trovato.");
             yield break;
         }
 
         float counter = 0;
 
-        // Imposta il valore iniziale della dissolvenza
         meshMaterial.SetFloat("_dissolve_amount", counter);
 
-        // Continua a incrementare il valore di dissolvenza fino a 1
+
         while (counter < 1)
         {
             counter += dissolveRate;
             meshMaterial.SetFloat("_dissolve_amount", counter);
             yield return new WaitForSeconds(refreshRate);
         }
+
+        Debug.Log(counter);
+
+        while (counter > 0)
+        {
+
+            Debug.Log("torno indietro");
+            counter -= dissolveRate;
+            counter = Mathf.Clamp01(counter);
+            meshMaterial.SetFloat("_dissolve_amount", counter);
+            for (int i = 0; i < sceneLights.Length; i++)
+            {
+                for (int j = 0; j < originalIntensities[i]; j++)
+                    sceneLights[i].intensity += dissolveRate;
+            }
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+ 
     }
 }
